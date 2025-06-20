4b998e88a1187099b5c9467e4cfb972eac91c21e
Sri Aarshitha <sriaarshitha_22ecb48@kgkite.ac.in>
2025-06-20 12:37:49 +0200

    Create loops.c

---
+++ day2/loops.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int i;
+
+
+int main() {
+    for (int i = 0; i <=5; i++){
+        print("%d\n",i);
+        }
+    i=0
+    do{
+        printf("%d\n",i);
+        i++;}
+    while(i <=5);
+    return 0;
+    }
+    }
