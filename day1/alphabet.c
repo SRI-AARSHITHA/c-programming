6fee2cf317bccc3a7d87d014267e28a2e03e0594
Sri Aarshitha <sriaarshitha_22ecb48@kgkite.ac.in>
2025-06-20 12:36:03 +0200

    Create alphabet.c

---
+++ day1/alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char val= 'S';
+    
+    
+    printf("%d %d %d %d",'a','z','A','Z');
+    if (val>=65 && val<=90 || val>= 97 && val<= 122)
+    {
+        printf("Alphabet");
+    }
+        
+    else 
+    {
+            
+        printf("not an alphabet");
+    
+    }
+
+    return 0;
+}
